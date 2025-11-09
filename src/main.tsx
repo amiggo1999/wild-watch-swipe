7c814ccd09985f1bc0b792c96ae1b943cd8b756c
thunder-energy <72627327+thunder-energy@users.noreply.github.com>
2025-11-09 11:19:10 +0100

    reset local storage when side loads

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import { resetLocalStorage } from './utils/localStorage'
+
+// Lösche Local Storage beim initialen Laden der Seite
+resetLocalStorage()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
